709b2be59f295250ea9ca5be2b1c427c6654675f
kononiaka <kononiaka.vladimir@gmail.com>
2020-02-25 13:18:49 +0200

    edited to  stateless component

--- src/components/Titles.jsx
+++ src/components/Titles.jsx
@@ -1,14 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Titles extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <h1>Weather Finder</h1>
-        <p>Find out weather, humidity, conditions and more...</p>
-      </React.Fragment>
-    );
-  }
-}
+const Titles = () => (
+  <React.Fragment>
+    <h1>Weather Finder</h1>
+    <p>Find out weather, humidity, conditions and more...</p>
+  </React.Fragment>
+);
 
 export default Titles;
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,21 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Weather extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        {this.props.city && this.props.country && (
-          <p>
-            City: {this.props.city}, {this.props.country}
-          </p>
-        )}
-        {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
-        {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
-        {this.props.description && <p>Condition: {this.props.description}</p>}
-        {this.props.error && <p>Error: {this.props.error}</p>}
-      </React.Fragment>
-    );
-  }
-}
+const Weather = props => (
+  <React.Fragment>
+    {props.city && props.country && (
+      <p>
+        City: {props.city}, {props.country}
+      </p>
+    )}
+    {props.temperature && <p>Temperature: {props.temperature}</p>}
+    {props.humidity && <p>Humidity: {props.humidity}</p>}
+    {props.description && <p>Condition: {props.description}</p>}
+    {props.error && <p>Error: {props.error}</p>}
+  </React.Fragment>
+);
 
 export default Weather;
